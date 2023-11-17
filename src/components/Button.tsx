aa8a166b6e3d7155acc3f38f6304f0b82cf70aee
Júnior Nascimento <junior.nascm@gmail.com>
2023-11-17 12:06:11 -0300

    init

---
+++ src/components/Button.tsx
@@ -0,0 +1,16 @@
+interface ButtonProps {
+  label: string;
+  secondary?: boolean;
+}
+
+export default function Button({ label, secondary }: ButtonProps) {
+  const baseClasses = 'font-manrope font-bold text-body px-6 py-4 cursor-pointer transition-all';
+  const primaryClasses = 'bg-eucaplyptus text-dark-purple hover:bg-dark-purple hover:border-2 hover:border-eucaplyptus hover:text-eucaplyptus';
+  const secondaryClasses = 'decoration-eucaplyptus decoration-4 underline underline-offset-8 decoration-th hover:text-eucaplyptus';
+  
+  return (
+    <button className={`${baseClasses} ${secondary ? secondaryClasses : primaryClasses}`}>
+      {label}
+    </button>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,10 +8,16 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        'white': '#FFFFFF',
+        'ghost-white': '#FCF8FF',
+        'davys-grey': '#584D62',
+        'eucaplyptus': '#44FFA1',
+        'dark-purple': '#24053E',
+      },
+      fontFamily: {
+        'manrope': 'Manrope',
+        'fraunces': 'Fraunces',
       },
     },
   },
